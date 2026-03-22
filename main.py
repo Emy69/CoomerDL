006d0a03676521f66e14a60d3b290cd5c653b680
Emy69 <viejone83@gmail.com>
2026-03-22 01:18:07 -0700

    Set application icon for window and executable

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 from PySide6.QtWidgets import QApplication
-
+from PySide6.QtGui import QIcon
 from app.views.pyside.main_window import PySideMainWindow
 
 
 def main():
     app = QApplication(sys.argv)
+    
+    app_icon = QIcon("resources/img/icono.ico")
+    app.setWindowIcon(app_icon)
+    
     window = PySideMainWindow()
     window.show()
     sys.exit(app.exec())
